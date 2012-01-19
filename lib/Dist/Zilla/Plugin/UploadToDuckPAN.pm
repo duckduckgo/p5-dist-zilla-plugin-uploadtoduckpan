290adca225b4b295dca50ef11ffeaf6604f20bd7
Torsten Raudssus <github@raudssus.de>
2012-01-19 02:36:13 +0100

    Requirement for SSL, more documentation

--- lib/Dist/Zilla/Plugin/UploadToDuckPAN.pm
+++ lib/Dist/Zilla/Plugin/UploadToDuckPAN.pm
@@ -1,4 +1,5 @@
 package Dist::Zilla::Plugin::UploadToDuckPAN;
+# ABSTRACT: Dist::Zilla plugin to upload to https://duckpan.org/ via https://dukgo.com/
 
 use Moose;
 extends 'Dist::Zilla::Plugin::UploadToCPAN';
@@ -15,3 +16,38 @@ has 'upload_uri' => (
 sub has_upload_uri { 1 }
 
 1;
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+In dist.ini:
+
+  [UploadToDuckPAN]
+  
+In ~/.dzil/config:
+
+  [%DUKGO]
+  username = youruserondukgocom
+  password = yourpasswordondukgo.com
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+L<Dist::Zilla::Plugin::UploadToCPAN>
+
+=head1 SUPPORT
+
+IRC
+
+  Join #duckduckgo on irc.freenode.net. Highlight Getty for fast reaction :).
+
+Repository
+
+  http://github.com/duckduckgo/p5-dist-zilla-plugin-uploadtoduckpan
+  Pull request and additional contributors are welcome
+ 
+Issue Tracker
+
+  http://github.com/duckduckgo/p5-dist-zilla-plugin-uploadtoduckpan/issues
--- lib/Dist/Zilla/Stash/DUKGO.pm
+++ lib/Dist/Zilla/Stash/DUKGO.pm
@@ -1,6 +1,28 @@
 package Dist::Zilla::Stash::DUKGO;
+# ABSTRACT: Stash for handling %DUKGO credentials
 
 use Moose;
 extends 'Dist::Zilla::Stash::PAUSE';
 
 1;
+
+=encoding utf8
+
+=head1 SEE ALSO
+
+L<Dist::Zilla::Stash::PAUSE>
+
+=head1 SUPPORT
+
+IRC
+
+  Join #duckduckgo on irc.freenode.net. Highlight Getty for fast reaction :).
+
+Repository
+
+  http://github.com/duckduckgo/p5-dist-zilla-plugin-uploadtoduckpan
+  Pull request and additional contributors are welcome
+ 
+Issue Tracker
+
+  http://github.com/duckduckgo/p5-dist-zilla-plugin-uploadtoduckpan/issues
