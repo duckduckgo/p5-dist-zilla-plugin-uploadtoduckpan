72b21537bdf5e016a8111f4f82b6e228f5e94373
Torsten Raudssus <torsten@raudss.us>
2013-02-11 00:13:46 +0100

    Fixed the deeper texts of UploadToCPAN to replace PAUSE with dukgo.com

--- lib/Dist/Zilla/Plugin/UploadToDuckPAN.pm
+++ lib/Dist/Zilla/Plugin/UploadToDuckPAN.pm
@@ -4,6 +4,8 @@ package Dist::Zilla::Plugin::UploadToDuckPAN;
 use Moose;
 extends 'Dist::Zilla::Plugin::UploadToCPAN';
 
+use Scalar::Util qw(weaken);
+
 has '+credentials_stash' => (
 	default => sub { '%DUKGO' }
 );
@@ -15,6 +17,46 @@ has 'upload_uri' => (
 );
 sub has_upload_uri { 1 }
 
+has '+username' => (
+  default  => sub {
+    my ($self) = @_;
+    return $self->_credential('username')
+        || $self->pause_cfg->{user}
+        || $self->zilla->chrome->prompt_str("dukgo.com username: ");
+  },
+);
+ 
+ 
+has '+password' => (
+  default  => sub {
+    my ($self) = @_;
+    return $self->_credential('password')
+        || $self->pause_cfg->{password}
+        || $self->zilla->chrome->prompt_str('dukgo.com password: ', { noecho => 1 });
+  },
+);
+
+has '+uploader' => (
+  default => sub {
+    my ($self) = @_;
+ 
+    my $uploader = Dist::Zilla::Plugin::UploadToCPAN::_Uploader->new({
+      user     => $self->username,
+      password => $self->password,
+      ($self->has_subdir
+           ? (subdir => $self->subdir) : ()),
+      ($self->has_upload_uri
+           ? (upload_uri => $self->upload_uri) : ()),
+      target => 'dukgo.com',
+    });
+ 
+    $uploader->{'Dist::Zilla'}{plugin} = $self;
+    weaken $uploader->{'Dist::Zilla'}{plugin};
+ 
+    return $uploader;
+  }
+);
+ 
 1;
 
 =encoding utf8
